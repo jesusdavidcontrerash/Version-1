2d814420aad6f7c59aa1d7278eb5ed7e14077e14
Jesùs Contreras <jesusdavidcontrerash@gmail.com>
2025-03-13 22:29:35 -0500

    version2

---
+++ src/main/java/com/mycompany/corfinia/Corfinia.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.corfinia;
+
+/**
+ *
+ * @author JesusContreras
+ */
+public class Corfinia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a = 5;
+        int b = 6;
+        System.out.println(a+b);
+    }
+}
